b1a5bb5ae5c44a4debcea876a6afe9cd6cf28583
easy-ing <easying1020@gmail.com>
2025-12-24 08:44:16 +0900

    [level 0] Title: 더 크게 합치기, Time: 14.09 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int a, int b) {
+        int ab = Integer.parseInt(String.valueOf(a) + b);
+        int ba = Integer.parseInt(String.valueOf(b) + a);
+
+        return (ab >= ba) ? ab : ba;
+    }
+}
