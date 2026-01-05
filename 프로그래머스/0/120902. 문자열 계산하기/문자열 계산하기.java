75ffeee5be6e1186a3d18c26dddc4cd40fd19671
easy-ing <easying1020@gmail.com>
2026-01-06 08:52:22 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.11 ms, Memory: 71.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(String my_string) {
+        String[] tokens = my_string.split(" "); 
+        int result = Integer.parseInt(tokens[0]);
+
+        for (int i = 1; i < tokens.length; i += 2) {
+            String op = tokens[i];
+            int num = Integer.parseInt(tokens[i + 1]);
+            if (op.equals("+")) {
+                result += num;
+            } else {
+                result -= num;
+            }
+        }
+
+        return result;
+    }
+}
