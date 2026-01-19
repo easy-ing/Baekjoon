c713b4669555972783b2a3229e8afefb8f9f1d0f
easy-ing <easying1020@gmail.com>
2026-01-20 08:49:06 +0900

    [level 0] Title: A로 B 만들기, Time: 1.20 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(String before, String after) {
+        char[] a = before.toCharArray();
+        char[] b = after.toCharArray();
+
+        Arrays.sort(a);
+        Arrays.sort(b);
+        return Arrays.equals(a, b) ? 1 : 0;
+    }
+}
