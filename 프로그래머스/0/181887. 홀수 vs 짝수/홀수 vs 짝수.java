563766ec59dc6e4fe430ecde729e6a4592405718
easy-ing <easying1020@gmail.com>
2026-01-22 08:36:38 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.06 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int oddSum = 0;   // 1,3,5번째 ...
+        int evenSum = 0;  // 2,4,6번째 ...
+
+        for (int i = 0; i < num_list.length; i++) {
+            if (i % 2 == 0) oddSum += num_list[i];
+            else evenSum += num_list[i];
+        }
+
+        return Math.max(oddSum, evenSum);
+    }
+}
