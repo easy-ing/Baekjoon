4fc78b09e15b8341bb9dfd4af9dadf16e2f0729c
easy-ing <easying1020@gmail.com>
2026-03-23 00:18:33 +0900

    [Silver III] Title: 계단 오르기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -0,0 +1,24 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+stairs = [0] * (n + 1)
+
+for i in range(1, n + 1):
+    stairs[i] = int(input())
+
+dp = [0] * (n + 1)
+
+if n >= 1:
+    dp[1] = stairs[1]
+if n >= 2:
+    dp[2] = stairs[1] + stairs[2]
+if n >= 3:
+    dp[3] = max(stairs[1] + stairs[3], stairs[2] + stairs[3])
+
+for i in range(4, n + 1):
+    dp[i] = max(dp[i - 2] + stairs[i],
+                dp[i - 3] + stairs[i - 1] + stairs[i])
+
+print(dp[n])
