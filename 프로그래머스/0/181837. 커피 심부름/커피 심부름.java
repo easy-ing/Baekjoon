55a56b18df48d65a358029fccdfa34bf7e807566
easy-ing <easying1020@gmail.com>
2026-03-28 09:29:26 +0900

    [level 0] Title: 커피 심부름, Time: 0.77 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
@@ -1,17 +1,13 @@
 class Solution {
     public int solution(String[] order) {
-        int total = 0;
-
-        for (String o : order) {
-            if (o.equals("anything")) {
-                total += 4500;
-            } else if (o.contains("americano")) {
-                total += 4500;
-            } else if (o.contains("cafelatte")) {
-                total += 5000;
+        int answer = 0;
+        for(String o : order) {
+            if(o.contains("cafelatte")) {
+                answer += 5000;
+            } else {
+                answer += 4500;
             }
         }
-
-        return total;
+        return answer;
     }
 }
