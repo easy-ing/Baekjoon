12a5eb8522bb6cbfe86cd262d2aa60807a421512
easy-ing <easying1020@gmail.com>
2026-04-03 21:41:42 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 520 ms, Memory: 55904 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -1,21 +1,14 @@
 import sys
+from bisect import bisect_left, bisect_right
 
 input = sys.stdin.readline
 
-# 문자열 입력
 S = input().strip()
-n = len(S)
 
-# 알파벳 26개에 대한 누적합 배열
-# prefix[i][c] : S의 앞에서부터 i개 문자까지 봤을 때 c의 등장 횟수
-prefix = [[0] * 26 for _ in range(n + 1)]
+positions = [[] for _ in range(26)]
 
-for i in range(n):
-    # 이전 값 복사
-    for c in range(26):
-        prefix[i + 1][c] = prefix[i][c]
-    # 현재 문자 반영
-    prefix[i + 1][ord(S[i]) - ord('a')] += 1
+for i, ch in enumerate(S):
+    positions[ord(ch) - ord('a')].append(i)
 
 q = int(input())
 answers = []
@@ -25,8 +18,8 @@ for _ in range(q):
     l = int(l)
     r = int(r)
 
-    idx = ord(alpha) - ord('a')
-    count = prefix[r + 1][idx] - prefix[l][idx]
+    arr = positions[ord(alpha) - ord('a')]
+    count = bisect_right(arr, r) - bisect_left(arr, l)
     answers.append(str(count))
 
 print('\n'.join(answers))
