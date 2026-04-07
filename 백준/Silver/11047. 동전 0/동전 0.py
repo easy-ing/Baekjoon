85b6d6fed6536a0c5c2f745247b48b1528b29260
easy-ing <easying1020@gmail.com>
2026-04-07 22:35:35 +0900

    [Silver IV] Title: 동전 0, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -0,0 +1,14 @@
+import sys
+
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
+coins = [int(input()) for _ in range(N)]
+
+count = 0
+
+for i in range(N - 1, -1, -1):
+    count += K // coins[i]
+    K %= coins[i]
+
+print(count)
