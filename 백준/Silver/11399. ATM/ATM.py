12540c5537c7d8a052669550a3f21599a095ba87
easy-ing <easying1020@gmail.com>
2026-04-08 21:56:38 +0900

    [Silver IV] Title: ATM, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.py
@@ -0,0 +1,17 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+times = list(map(int, input().split()))
+
+times.sort()
+
+prefix = 0
+answer = 0
+
+for t in times:
+    prefix += t
+    answer += prefix
+
+print(answer)
