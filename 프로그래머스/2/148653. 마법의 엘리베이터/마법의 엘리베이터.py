6f66d32a2acecf8fc10704df1628eb49fd1ca833
easy-ing <easying1020@gmail.com>
2026-06-10 23:35:55 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.01 ms, Memory: 11.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.py
@@ -0,0 +1,24 @@
+def solution(storey):
+    answer = 0
+
+    while storey > 0:
+        digit = storey % 10
+        next_digit = (storey // 10) % 10
+
+        if digit < 5:
+            answer += digit
+            storey //= 10
+
+        elif digit > 5:
+            answer += 10 - digit
+            storey = storey // 10 + 1
+
+        else:
+            if next_digit >= 5:
+                answer += 5
+                storey = storey // 10 + 1
+            else:
+                answer += 5
+                storey //= 10
+
+    return answer
