a68cac4bce7093f7ec8ceb6f2311e22a9f8f0e1c
easy-ing <easying1020@gmail.com>
2026-07-14 00:02:19 +0900

    [level 1] Title: 2016년, Time: 0.00 ms, Memory: 11.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12901. 2016년/2016년.py
@@ -0,0 +1,10 @@
+def solution(a, b):
+    days = ["FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"]
+    month = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+    total = b - 1
+
+    for i in range(a - 1):
+        total += month[i]
+
+    return days[total % 7]
