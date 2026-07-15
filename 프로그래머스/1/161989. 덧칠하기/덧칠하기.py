b68148cca07f3a28d8afd311205ce416cbaa8ddd
easy-ing <easying1020@gmail.com>
2026-07-15 21:45:02 +0900

    [level 1] Title: 덧칠하기, Time: 2.59 ms, Memory: 13.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
@@ -0,0 +1,10 @@
+def solution(n, m, section):
+    answer = 0
+    painted_until = 0
+
+    for area in section:
+        if area > painted_until:
+            answer += 1
+            painted_until = area + m - 1
+
+    return answer
