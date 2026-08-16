67e7d2c8f47acfb0336de48c96c22881d9145c7c
easy-ing <easying1020@gmail.com>
2026-08-16 15:14:40 +0900

    [level 2] Title: 방문 길이, Time: 0.14 ms, Memory: 11.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.py
@@ -0,0 +1,21 @@
+def solution(dirs):
+    move = {
+        'U': (-1, 0),
+        'D': (1, 0),
+        'L': (0, -1),
+        'R': (0, 1)
+    }
+
+    x, y = 0, 0
+    visited = set()
+
+    for d in dirs:
+        dx, dy = move[d]
+        nx, ny = x + dx, y + dy
+
+        if -5 <= nx <= 5 and -5 <= ny <= 5:
+            visited.add((x, y, nx, ny))
+            visited.add((nx, ny, x, y))
+            x, y = nx, ny
+
+    return len(visited) // 2
